6a264c85bb01f139300d3115dcdec2b535d297cf
Ciro-Ugalde <ciro.daniel.ugalde@gmail.com>
2022-05-24 22:01:46 -0300

    aula sobre herança

--- Orientacao a objetos/src/br/com/entra21/orientacao/objetos/principal/aula02/heranca/Diretor.java
+++ Orientacao a objetos/src/br/com/entra21/orientacao/objetos/principal/aula02/heranca/Diretor.java
@@ -1,22 +1,66 @@
 package br.com.entra21.orientacao.objetos.principal.aula02.heranca;
 
-public class Diretor {
-	private String nome;
-	private byte idade;
+public class Diretor extends Pessoa {
+
+	// removi os atributos comuns
+	// private String nome;//
+	// private byte idade;//
+	// private String cpf;//
+
+	// mantive os especificos
 	private String cursoAtual;
 	private byte quantidadeProfessores;
 
 	public Diretor() {
-
+		super();
 	}
 
-	public Diretor(String nome, byte idade) {
-		this.nome = nome;
+	public Diretor(String nome, byte idade, String cpf, String cursoAtual, byte quantidadeProfessores) {
+		super(nome, idade, cpf);
+		setNome(nome);
+		setIdade(idade);
+		this.cursoAtual = cursoAtual;
+		this.quantidadeProfessores = quantidadeProfessores;
+	}
 
-		this.idade = idade;
+	// removi os metodos comuns
+	/*
+	 * public String getNome() { return nome; }
+	 * 
+	 * 
+	 * public void setNome(String nome) { this.nome = nome; }
+	 * 
+	 * 
+	 * public byte getIdade() { return idade; }
+	 * 
+	 * 
+	 * public void setIdade(byte idade) { this.idade = idade; }
+	 * 
+	 * 
+	 * public String getCpf() { return cpf; }
+	 * 
+	 * 
+	 * public void setCpf(String cpf) { this.cpf = cpf; }
+	 */
+
+	public String getCursoAtual() {
+		return cursoAtual;
 	}
-	
-	public void realizarApresentacao() {
-		System.out.println("Oi meu nome é "+this.nome+ " e tenho " +this.idade);
+
+	public void setCursoAtual(String cursoAtual) {
+		this.cursoAtual = cursoAtual;
 	}
+
+	public byte getQuantidadeProfessores() {
+		return quantidadeProfessores;
+	}
+
+	public void setQuantidadeProfessores(byte quantidadeProfessores) {
+		this.quantidadeProfessores = quantidadeProfessores;
+	}
+
+	public void realizarParesentacao() {
+		System.out.println("Oi meu nome é " + getNome() + " tenho " + getIdade());
+	}
+
 }
